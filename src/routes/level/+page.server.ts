9cfedc782d74c43a2463f272008af288705a506f
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-08 22:14:33 +0500

    questions

--- src/routes/level/+page.server.ts
+++ src/routes/level/+page.server.ts
@@ -1,4 +1,14 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Question } from "./$types";
+
+function level1() {
+    const level1: Array<Question> = [
+        {
+            question: "hehe",
+            options: ["linkin park","bmth","paramore","jeje"],
+            right: "bmth"
+        }
+    ];
+}
 
 export const load = (() => {
     return {
