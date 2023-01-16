d0b09502a49658373e5f5ffa98781ef76460d8c0
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-16 15:01:03 +0500

    update api

--- src/routes/api/level/+server.ts
+++ src/routes/api/level/+server.ts
@@ -1,14 +1,22 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
-import { level1 } from './levels';
+import { level1, level2, level3, level4, level5 } from './levels';
  
 export const GET: RequestHandler = async ({ url }) => {
   const level: Number = Number(url.searchParams.get('level') ?? '1');
 
-  if (level > 5) {
-     throw error(404, "level not found");
+  if (level == 1) {
+    return json(level1());
+  } else if (level == 2) {
+    return json(level2()) 
+  } else if (level == 3) {
+    return json(level3()) 
+  } else if (level == 4) {
+    return json(level4()) 
+  } else if (level == 5) {
+    return json(level5()) 
   }
-  return json(level1());
+  throw error(404, "level not found");
 }
 
